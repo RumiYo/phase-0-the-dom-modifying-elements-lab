7f0506d651ccd5443dfbccb86456afa69dcbb470
RumiYo <yokoyamarumi@gmail.com>
2023-06-04 14:14:42 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id","victory");
+newHeader.textContent = "Rumi Yokoyama is the champion"
+document.body.append("newHeader");
